90d4239fecf587c61b40d633342545ee8385b8e5
jumpiix <david@unterguggenberger.ch>
2025-05-05 16:04:27 +0200

    refactor: adjustments comments

--- src/api/graphql/scalars/datetime.rs
+++ src/api/graphql/scalars/datetime.rs
@@ -4,7 +4,7 @@ use async_graphql::{
 };
 use sqlx::types::chrono::{DateTime as ChronoDateTime, Utc};
 
-/// `DateTime` scalar representing RFC3339 formatted date-times
+
 #[derive(Clone, Debug)]
 pub struct DateTime(pub ChronoDateTime<Utc>);
 
@@ -38,7 +38,7 @@ impl From<DateTime> for ChronoDateTime<Utc> {
     }
 }
 
-/// Register the `DateTime` scalar with the schema
+
 pub fn register(
     builder: SchemaBuilder<QueryRoot, EmptyMutation, SubscriptionRoot>,
 ) -> SchemaBuilder<QueryRoot, EmptyMutation, SubscriptionRoot> {
