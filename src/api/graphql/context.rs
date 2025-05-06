d43878f50021e617bdefc059927cc36e9dcac313
jumpiix <david@unterguggenberger.ch>
2025-05-06 16:46:30 +0200

    cargo fmt

--- src/api/graphql/context.rs
+++ src/api/graphql/context.rs
@@ -1,14 +1,11 @@
 use sqlx::PgPool;
 
-
 #[allow(clippy::module_name_repetitions)]
 pub struct ApiContext {
-    
     pub db: PgPool,
 }
 
 impl ApiContext {
-    
     #[must_use]
     pub fn new(db: PgPool) -> Self {
         Self { db }
