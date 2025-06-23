6617109e593cdb97a36eadaea7cecda0d297e4f8
jumpiix <david@unterguggenberger.ch>
2025-06-23 17:16:36 +0200

    Added DexStats graphql

--- src/api/graphql/resolvers/dex.rs
+++ src/api/graphql/resolvers/dex.rs
@@ -87,8 +87,23 @@ pub async fn resolve_latest_executions(
     filter: Option<SwapExecutionFilter>,
 ) -> Result<Vec<SwapExecution>> {
     let db = &ctx.data_unchecked::<ApiContext>().db;
+    let block_heights = fetch_block_heights(db, filter).await?;
 
-    let block_heights = if let Some(filter) = &filter {
+    let mut swap_executions = Vec::new();
+    for block_height in block_heights {
+        if let Some(execution) = fetch_swap_execution(db, block_height).await? {
+            swap_executions.push(execution);
+        }
+    }
+
+    Ok(swap_executions)
+}
+
+async fn fetch_block_heights(
+    db: &sqlx::PgPool,
+    filter: Option<SwapExecutionFilter>,
+) -> Result<Vec<i64>> {
+    if let Some(filter) = filter {
         if let Some(height) = filter.height {
             let exists: bool = sqlx::query_scalar(
                 "SELECT EXISTS(SELECT 1 FROM dex_batch_swaps WHERE block_height = $1)",
@@ -97,157 +112,158 @@ pub async fn resolve_latest_executions(
             .fetch_one(db)
             .await?;
 
-            if exists {
-                vec![height]
-            } else {
-                vec![]
-            }
-        } else {
-            sqlx::query_scalar::<_, i64>(
-                r"
-                SELECT DISTINCT block_height 
-                FROM dex_batch_swaps 
-                ORDER BY block_height DESC 
-                LIMIT 10
-                ",
-            )
-            .fetch_all(db)
-            .await?
+            return Ok(if exists { vec![height] } else { vec![] });
         }
-    } else {
-        sqlx::query_scalar::<_, i64>(
-            r"
-            SELECT DISTINCT block_height 
-            FROM dex_batch_swaps 
-            ORDER BY block_height DESC 
-            LIMIT 10
-            ",
-        )
-        .fetch_all(db)
-        .await?
-    };
+    }
 
-    let mut swap_executions = Vec::new();
+    sqlx::query_scalar::<_, i64>(
+        r"
+        SELECT DISTINCT block_height 
+        FROM dex_batch_swaps 
+        ORDER BY block_height DESC 
+        LIMIT 10
+        ",
+    )
+    .fetch_all(db)
+    .await
+    .map_err(Into::into)
+}
 
-    for block_height in block_heights {
-        let batch_swap_rows = sqlx::query(
-            r"
-            SELECT 
-                bs.id,
-                bs.block_timestamp,
-                bs.execution_type,
-                bs.total_input_amount::TEXT as total_input_amount,
-                bs.total_input_asset_id,
-                bs.total_output_amount::TEXT as total_output_amount,
-                bs.total_output_asset_id,
-                bs.individual_swaps_count
-            FROM 
-                dex_batch_swaps bs
-            WHERE 
-                bs.block_height = $1
-            ORDER BY 
-                bs.id ASC
-            ",
-        )
-        .bind(block_height)
-        .fetch_all(db)
-        .await?;
+async fn fetch_swap_execution(
+    db: &sqlx::PgPool,
+    block_height: i64,
+) -> Result<Option<SwapExecution>> {
+    let batch_swap_rows = sqlx::query(
+        r"
+        SELECT 
+            bs.id,
+            bs.block_timestamp,
+            bs.execution_type,
+            bs.total_input_amount::TEXT as total_input_amount,
+            bs.total_input_asset_id,
+            bs.total_output_amount::TEXT as total_output_amount,
+            bs.total_output_asset_id,
+            bs.individual_swaps_count
+        FROM 
+            dex_batch_swaps bs
+        WHERE 
+            bs.block_height = $1
+        ORDER BY 
+            bs.id ASC
+        ",
+    )
+    .bind(block_height)
+    .fetch_all(db)
+    .await?;
 
-        if batch_swap_rows.is_empty() {
-            continue;
-        }
+    if batch_swap_rows.is_empty() {
+        return Ok(None);
+    }
 
-        let timestamp =
-            batch_swap_rows[0].get::<chrono::DateTime<chrono::Utc>, _>("block_timestamp");
+    let timestamp = batch_swap_rows[0].get::<chrono::DateTime<chrono::Utc>, _>("block_timestamp");
+    let mut batch_swaps = Vec::new();
 
-        let mut batch_swaps = Vec::new();
+    for batch_row in batch_swap_rows {
+        let batch_swap = fetch_batch_swap(db, &batch_row).await?;
+        batch_swaps.push(batch_swap);
+    }
 
-        for batch_row in batch_swap_rows {
-            let batch_swap_id: i32 = batch_row.get("id");
+    Ok(Some(SwapExecution {
+        block_height,
+        timestamp: DateTime(timestamp),
+        batch_swaps,
+    }))
+}
 
-            let individual_swap_rows = sqlx::query(
-                r"
-                SELECT 
-                    id,
-                    swap_index,
-                    input_amount::TEXT as input_amount,
-                    input_asset_id,
-                    output_amount::TEXT as output_amount,
-                    output_asset_id
-                FROM 
-                    dex_individual_swaps
-                WHERE 
-                    batch_swap_id = $1
-                ORDER BY 
-                    swap_index ASC
-                ",
-            )
-            .bind(batch_swap_id)
-            .fetch_all(db)
-            .await?;
+async fn fetch_batch_swap(
+    db: &sqlx::PgPool,
+    batch_row: &sqlx::postgres::PgRow,
+) -> Result<BatchSwap> {
+    let batch_swap_id: i32 = batch_row.get("id");
+    let individual_swaps = fetch_individual_swaps(db, batch_swap_id).await?;
 
-            let mut individual_swaps = Vec::new();
+    Ok(BatchSwap {
+        id: batch_swap_id,
+        execution_type: batch_row.get("execution_type"),
+        total_input_amount: batch_row.get("total_input_amount"),
+        total_input_asset_id: batch_row.get("total_input_asset_id"),
+        total_output_amount: batch_row.get("total_output_amount"),
+        total_output_asset_id: batch_row.get("total_output_asset_id"),
+        individual_swaps_count: batch_row.get("individual_swaps_count"),
+        individual_swaps,
+    })
+}
 
-            for swap_row in individual_swap_rows {
-                let individual_swap_id: i32 = swap_row.get("id");
+async fn fetch_individual_swaps(
+    db: &sqlx::PgPool,
+    batch_swap_id: i32,
+) -> Result<Vec<IndividualSwap>> {
+    let individual_swap_rows = sqlx::query(
+        r"
+        SELECT 
+            id,
+            swap_index,
+            input_amount::TEXT as input_amount,
+            input_asset_id,
+            output_amount::TEXT as output_amount,
+            output_asset_id
+        FROM 
+            dex_individual_swaps
+        WHERE 
+            batch_swap_id = $1
+        ORDER BY 
+            swap_index ASC
+        ",
+    )
+    .bind(batch_swap_id)
+    .fetch_all(db)
+    .await?;
 
-                let route_step_rows = sqlx::query(
-                    r"
-                    SELECT 
-                        route_step,
-                        amount::TEXT as amount,
-                        asset_id
-                    FROM 
-                        dex_individual_swap_routes
-                    WHERE 
-                        individual_swap_id = $1
-                    ORDER BY 
-                        route_step ASC
-                    ",
-                )
-                .bind(individual_swap_id)
-                .fetch_all(db)
-                .await?;
+    let mut individual_swaps = Vec::new();
+    for swap_row in individual_swap_rows {
+        let individual_swap_id: i32 = swap_row.get("id");
+        let route_steps = fetch_route_steps(db, individual_swap_id).await?;
 
-                let route_steps: Vec<RouteStep> = route_step_rows
-                    .into_iter()
-                    .map(|route_row| RouteStep {
-                        route_step: route_row.get("route_step"),
-                        asset_id: route_row.get("asset_id"),
-                        amount: route_row.get("amount"),
-                    })
-                    .collect();
-
-                individual_swaps.push(IndividualSwap {
-                    swap_index: swap_row.get("swap_index"),
-                    input_amount: swap_row.get("input_amount"),
-                    input_asset_id: swap_row.get("input_asset_id"),
-                    output_amount: swap_row.get("output_amount"),
-                    output_asset_id: swap_row.get("output_asset_id"),
-                    route_steps,
-                });
-            }
-
-            batch_swaps.push(BatchSwap {
-                id: batch_swap_id,
-                execution_type: batch_row.get("execution_type"),
-                total_input_amount: batch_row.get("total_input_amount"),
-                total_input_asset_id: batch_row.get("total_input_asset_id"),
-                total_output_amount: batch_row.get("total_output_amount"),
-                total_output_asset_id: batch_row.get("total_output_asset_id"),
-                individual_swaps_count: batch_row.get("individual_swaps_count"),
-                individual_swaps,
-            });
-        }
-
-        swap_executions.push(SwapExecution {
-            block_height,
-            timestamp: DateTime(timestamp),
-            batch_swaps,
+        individual_swaps.push(IndividualSwap {
+            swap_index: swap_row.get("swap_index"),
+            input_amount: swap_row.get("input_amount"),
+            input_asset_id: swap_row.get("input_asset_id"),
+            output_amount: swap_row.get("output_amount"),
+            output_asset_id: swap_row.get("output_asset_id"),
+            route_steps,
         });
     }
 
-    Ok(swap_executions)
+    Ok(individual_swaps)
+}
+
+async fn fetch_route_steps(db: &sqlx::PgPool, individual_swap_id: i32) -> Result<Vec<RouteStep>> {
+    let route_step_rows = sqlx::query(
+        r"
+        SELECT 
+            route_step,
+            amount::TEXT as amount,
+            asset_id
+        FROM 
+            dex_individual_swap_routes
+        WHERE 
+            individual_swap_id = $1
+        ORDER BY 
+            route_step ASC
+        ",
+    )
+    .bind(individual_swap_id)
+    .fetch_all(db)
+    .await?;
+
+    Ok(route_step_rows
+        .into_iter()
+        .map(|route_row| RouteStep {
+            route_step: route_row.get("route_step"),
+            asset_id: route_row.get("asset_id"),
+            amount: route_row.get("amount"),
+        })
+        .collect())
 }
 
 /// Resolves DEX statistics
